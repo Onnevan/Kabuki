c33977c83d3c2765dbdab9166724c9ff9a553f04
Onnevan <48829205+Onnevan@users.noreply.github.com>
2026-09-26 00:46:46 +0200

    Make 3D gizmo axes individually pickable

--- editor/transform_gizmo.gd
+++ editor/transform_gizmo.gd
@@ -2,87 +2,91 @@ class_name TransformGizmo
 extends Node3D
 
 enum Mode { MOVE, ROTATE, SCALE }
+enum Axis { NONE=-1, X=0, Y=1, Z=2 }
 var mode := Mode.MOVE
 var target: Node3D
 var parts: Array[Node3D] = []
 var mats: Array[StandardMaterial3D] = []
+var active_axis := Axis.NONE
 
 func _ready() -> void:
-	mats = [_mat(Color(0.92,0.18,0.20)), _mat(Color(0.24,0.82,0.34)), _mat(Color(0.18,0.46,0.96))]
-	_rebuild()
-	visible = false
+	mats=[_mat(Color("#ff4d5a")),_mat(Color("#57d47b")),_mat(Color("#4f86ff"))]
+	_rebuild(); visible=false
 
-func _mat(c: Color) -> StandardMaterial3D:
-	var m := StandardMaterial3D.new()
-	m.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	m.albedo_color = c
-	m.no_depth_test = true
-	return m
-
-func _mesh(mesh: Mesh, mat: Material) -> MeshInstance3D:
-	var n := MeshInstance3D.new(); n.mesh = mesh; n.material_override = mat; add_child(n); parts.append(n); return n
-
-func _clear() -> void:
+func _mat(c:Color)->StandardMaterial3D:
+	var m:=StandardMaterial3D.new();m.shading_mode=BaseMaterial3D.SHADING_MODE_UNSHADED;m.albedo_color=c;m.no_depth_test=true;return m
+func _mesh(me:Mesh,ma:Material,axis:int)->MeshInstance3D:
+	var n:=MeshInstance3D.new();n.mesh=me;n.material_override=ma;n.set_meta("axis",axis);add_child(n);parts.append(n);return n
+func _clear()->void:
 	for n in parts:
-		if is_instance_valid(n): n.queue_free()
+		if is_instance_valid(n):n.queue_free()
 	parts.clear()
-
-func _axis_basis(axis: int, n: Node3D) -> void:
-	if axis == 0: n.rotation.z = -PI/2.0
-	elif axis == 2: n.rotation.x = PI/2.0
-
-func _rebuild() -> void:
+func _orient(axis:int,n:Node3D)->void:
+	if axis==0:n.rotation.z=-PI/2.0
+	elif axis==2:n.rotation.x=PI/2.0
+func _rebuild()->void:
 	_clear()
-	if mode == Mode.MOVE: _build_move()
-	elif mode == Mode.SCALE: _build_scale()
-	else: _build_rotate()
-
-func _build_move() -> void:
-	for axis in 3:
-		var shaft := CylinderMesh.new(); shaft.top_radius=0.018; shaft.bottom_radius=0.018; shaft.height=0.72
-		var s := _mesh(shaft,mats[axis]); _axis_basis(axis,s)
-		if axis==0:s.position.x=0.36
-		elif axis==1:s.position.y=0.36
-		else:s.position.z=0.36
-		var cone:=CylinderMesh.new(); cone.top_radius=0.0; cone.bottom_radius=0.07; cone.height=0.18
-		var h:=_mesh(cone,mats[axis]); _axis_basis(axis,h)
-		if axis==0:h.position.x=0.81
-		elif axis==1:h.position.y=0.81
-		else:h.position.z=0.81
-
-func _build_scale() -> void:
-	for axis in 3:
-		var shaft:=CylinderMesh.new(); shaft.top_radius=0.016; shaft.bottom_radius=0.016; shaft.height=0.72
-		var s:=_mesh(shaft,mats[axis]); _axis_basis(axis,s)
-		if axis==0:s.position.x=0.36
-		elif axis==1:s.position.y=0.36
-		else:s.position.z=0.36
-		var cube:=BoxMesh.new(); cube.size=Vector3(0.12,0.12,0.12)
-		var h:=_mesh(cube,mats[axis])
-		if axis==0:h.position.x=0.78
-		elif axis==1:h.position.y=0.78
-		else:h.position.z=0.78
-
-func _build_rotate() -> void:
-	for axis in 3:
-		var torus:=TorusMesh.new(); torus.inner_radius=0.47; torus.outer_radius=0.51; torus.rings=64; torus.ring_segments=8
-		var r:=_mesh(torus,mats[axis])
-		if axis==0:r.rotation.z=PI/2.0
-		elif axis==2:r.rotation.x=PI/2.0
-
-func attach(node: Node3D) -> void:
-	target=node; visible=target!=null; _sync()
-
-func set_mode(new_mode: Mode) -> void:
-	if mode==new_mode:return
-	mode=new_mode; _rebuild()
-
-func _process(_delta: float) -> void:_sync()
-
-func _sync() -> void:
+	if mode==Mode.MOVE:_build_move()
+	elif mode==Mode.SCALE:_build_scale()
+	else:_build_rotate()
+func _build_move()->void:
+	for a in 3:
+		var sh:=CylinderMesh.new();sh.top_radius=.014;sh.bottom_radius=.014;sh.height=.68
+		var s:=_mesh(sh,mats[a],a);_orient(a,s)
+		var co:=CylinderMesh.new();co.top_radius=0.;co.bottom_radius=.065;co.height=.18
+		var h:=_mesh(co,mats[a],a);_orient(a,h)
+		if a==0:s.position.x=.34;h.position.x=.77
+		elif a==1:s.position.y=.34;h.position.y=.77
+		else:s.position.z=.34;h.position.z=.77
+func _build_scale()->void:
+	for a in 3:
+		var sh:=CylinderMesh.new();sh.top_radius=.014;sh.bottom_radius=.014;sh.height=.68
+		var s:=_mesh(sh,mats[a],a);_orient(a,s)
+		var bo:=BoxMesh.new();bo.size=Vector3(.115,.115,.115)
+		var h:=_mesh(bo,mats[a],a)
+		if a==0:s.position.x=.34;h.position.x=.76
+		elif a==1:s.position.y=.34;h.position.y=.76
+		else:s.position.z=.34;h.position.z=.76
+func _build_rotate()->void:
+	for a in 3:
+		var t:=TorusMesh.new();t.inner_radius=.47;t.outer_radius=.505;t.rings=64;t.ring_segments=8
+		var r:=_mesh(t,mats[a],a)
+		if a==0:r.rotation.z=PI/2.
+		elif a==2:r.rotation.x=PI/2.
+func attach(n:Node3D)->void:target=n;visible=target!=null;_sync()
+func set_mode(m:Mode)->void:
+	if mode==m:return
+	mode=m;_rebuild()
+func axis_vector(a:int)->Vector3:
+	return [Vector3.RIGHT,Vector3.UP,Vector3.BACK][a] if a>=0 and a<3 else Vector3.ZERO
+func pick_axis(mouse:Vector2,camera:Camera3D)->int:
+	if target==null:return Axis.NONE
+	var origin:=camera.unproject_position(global_position)
+	var best:=Axis.NONE;var best_d:=18.0
+	if mode==Mode.ROTATE:
+		var radius_world:=.5*scale.x
+		for a in 3:
+			for j in 48:
+				var ang:=TAU*float(j)/48.0
+				var p:=global_position
+				if a==0:p+=Vector3(0,cos(ang),sin(ang))*radius_world
+				elif a==1:p+=Vector3(cos(ang),0,sin(ang))*radius_world
+				else:p+=Vector3(cos(ang),sin(ang),0)*radius_world
+				var d:=camera.unproject_position(p).distance_to(mouse)
+				if d<best_d:best_d=d;best=a
+	else:
+		for a in 3:
+			var end:=camera.unproject_position(global_position+axis_vector(a)*.82*scale.x)
+			var d:=_point_segment_distance(mouse,origin,end)
+			if d<best_d:best_d=d;best=a
+	active_axis=best;return best
+func _point_segment_distance(p:Vector2,a:Vector2,b:Vector2)->float:
+	var ab:=b-a;var den:=ab.length_squared()
+	if den<.001:return p.distance_to(a)
+	var t:=clampf((p-a).dot(ab)/den,0.,1.);return p.distance_to(a+ab*t)
+func _process(_d:float)->void:_sync()
+func _sync()->void:
 	if target==null or not is_instance_valid(target):visible=false;return
-	visible=true; global_position=target.global_position; global_rotation=Vector3.ZERO
+	visible=true;global_position=target.global_position;global_rotation=Vector3.ZERO
 	var cam:=get_viewport().get_camera_3d()
-	if cam:
-		var d:=cam.global_position.distance_to(global_position)
-		scale=Vector3.ONE*maxf(0.10,d*0.105)
+	if cam:scale=Vector3.ONE*maxf(.10,cam.global_position.distance_to(global_position)*.105)
