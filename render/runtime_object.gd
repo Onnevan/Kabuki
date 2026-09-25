01249ecf244da6678b343abad39901bfb535a245
Onnevan <48829205+Onnevan@users.noreply.github.com>
2026-09-25 23:57:36 +0200

    Kabuki v0.2: direct canvas interaction foundation

--- render/runtime_object.gd
+++ render/runtime_object.gd
@@ -4,6 +4,7 @@ extends MeshInstance3D
 var model: MotionObject
 var texture: ImageTexture
 var material: ShaderMaterial
+var selected := false
 
 func setup(obj: MotionObject, img: Image) -> void:
 	model = obj
@@ -15,8 +16,19 @@ func setup(obj: MotionObject, img: Image) -> void:
 	material_override = material
 	name = obj.name
 
+func set_selected(value: bool) -> void:
+	selected = value
+	material.set_shader_parameter("selected", 1.0 if value else 0.0)
+
+func screen_radius(camera: Camera3D, viewport_size: Vector2i) -> float:
+	if mesh == null: return 30.0
+	var size := mesh.get_aabb().size
+	var center := camera.unproject_position(global_position)
+	var edge := camera.unproject_position(global_position + Vector3(maxf(size.x, size.y) * 0.5, 0, 0))
+	return maxf(20.0, center.distance_to(edge))
+
 func apply_frame(frame: int) -> void:
-	position = ProjectStore.evaluate(model.id, "transform.position", frame, position)
+	position = ProjectStore.evaluate(model.id, "transform.position", frame, model.transform.origin)
 	rotation = ProjectStore.evaluate(model.id, "transform.rotation", frame, rotation)
 	scale = ProjectStore.evaluate(model.id, "transform.scale", frame, scale)
 	material.set_shader_parameter("opacity", ProjectStore.evaluate(model.id,"effects.opacity",frame,1.0))
